7f8e3ae7835040fc496008ef7012e10925b4a669
Brandon Smith <vectorcell@gmail.com>
2016-08-01 14:41:19 -0500

    first change to see if setup works

--- Makefile
+++ Makefile
@@ -7,6 +7,7 @@ main.hex:
 
 load: main.hex
 	teensy_loader_cli -v -mmcu=mk20dx256 -w main.hex
+	rm main.hex
 
 clean:
 	cd lib ; make clean
--- main.cc
+++ main.cc
@@ -7,10 +7,17 @@ extern "C" int main(void)
 	// To use Teensy 3.0 without Arduino, simply put your code here.
 	// For example:
 
+	pinMode(16, OUTPUT);
+	pinMode(15, OUTPUT);
+	pinMode(14, OUTPUT);
 	pinMode(13, OUTPUT);
 	while (1) {
+		for (int pin = 14; pin <= 16; ++pin)
+			digitalWriteFast(pin, HIGH);
 		digitalWriteFast(13, HIGH);
 		delay(500);
+		for (int pin = 14; pin <= 16; ++pin)
+			digitalWriteFast(pin, LOW);
 		digitalWriteFast(13, LOW);
 		delay(500);
 	}
