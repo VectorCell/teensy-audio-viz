75491ee7f1fface1a02540060bb3c05d08833a9b
Brandon Smith <vectorcell@gmail.com>
2016-08-03 00:25:51 -0500

    added testing harness for testing on local machine (not teensy)

--- Makefile
+++ Makefile
@@ -7,9 +7,18 @@ main.hex: main.cc
 	cd lib ; make
 	cp lib/main.hex main.hex
 
+main.elf:
+	g++ -std=c++11 -O0 -Wall main.cc -o main.elf -DTESTING_MODE
+
+test: main.hex main.elf
+	timeout --preserve-status -k 2 1 ./main.elf
+
 load: main.hex
 	sudo $$(which teensy_loader_cli) -v -mmcu=mk20dx256 -w main.hex
-	rm main.hex
 
 clean:
+	rm -f main.hex
+	rm -f main.elf
+
+purge: clean
 	cd lib ; make clean
--- main.cc
+++ main.cc
@@ -1,32 +1,104 @@
-#include "WProgram.h"
+#ifdef TESTING_MODE
+	#include "test_harness.h"
+#else
+	#include "WProgram.h"
+#endif
 
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-const int P_LED = 13;
 
-vector<int> pins = {21, 22, 23};
+#define TWOPI (2 * PI)
+
+#define P_LED  13
+#define P_READ 21
+vector<int> pwm_pins = {20, 22, 23};
+
+#define N_SAMPLES 2048
+double sample[N_SAMPLES];
+
 
 void setup ()
 {
+	#ifdef TESTING_MODE
+		set_signal_handlers();
+	#endif
+
+	pinMode(P_READ,  INPUT);
+
 	pinMode(P_LED, OUTPUT);
-	for (int p : pins) {
+	for (int p : pwm_pins) {
 		pinMode(p, OUTPUT);
 	}
 }
+
+
+void gather_data (double *data, int n)
+{
+	while (--n >= 0) {
+		data[n] = analogRead(P_READ) / 1024.0;
+	}
+}
+
+
+void fft (double *data, int nn, int isign)
+{
+	int n, mmax, m, j, istep, i;
+	double wtemp, wr, wpr, wpi, wi, theta;
+	double tempr, tempi;
+	
+	n = nn << 1;
+	j = 1;
+	for (i = 1; i < n; i += 2) {
+		if (j > i) {
+			tempr = data[j];     data[j] = data[i];     data[i] = tempr;
+			tempr = data[j+1]; data[j+1] = data[i+1]; data[i+1] = tempr;
+		}
+		m = n >> 1;
+		while (m >= 2 && j > m) {
+			j -= m;
+			m >>= 1;
+		}
+		j += m;
+	}
+	mmax = 2;
+	while (n > mmax) {
+		istep = 2*mmax;
+		theta = TWOPI/(isign*mmax);
+		wtemp = sin(0.5*theta);
+		wpr = -2.0*wtemp*wtemp;
+		wpi = sin(theta);
+		wr = 1.0;
+		wi = 0.0;
+		for (m = 1; m < mmax; m += 2) {
+			for (i = m; i <= n; i += istep) {
+			j =i + mmax;
+			tempr = wr*data[j]   - wi*data[j+1];
+			tempi = wr*data[j+1] + wi*data[j];
+			data[j]   = data[i]   - tempr;
+			data[j+1] = data[i+1] - tempi;
+			data[i] += tempr;
+			data[i+1] += tempi;
+			}
+			wr = (wtemp = wr)*wpr - wi*wpi + wr;
+			wi = wi*wpr + wtemp*wpi + wi;
+		}
+		mmax = istep;
+	}
+}
+
 
 int main (void)
 {
 	setup();
 
-	unsigned int pass = 0;
-	int level = LOW;
 	while (true) {
-		if ((pass & 0xfffff) == 0) {
-			level = (level == LOW) ? HIGH : LOW;
-			digitalWriteFast(P_LED, level);
-		}
-		++pass;
+		
+		digitalWriteFast(P_LED, HIGH);
+		gather_data(sample, N_SAMPLES);
+		digitalWriteFast(P_LED, LOW);
+		//fft(sample, N_SAMPLES, 1);
 	}
 }
